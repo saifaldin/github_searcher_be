820c7de12c78930455da4789662d49a7b28cb4a0
saifaldin3388 <saifaldin3388@gmail.com>
2021-12-29 02:04:16 +0200

    finalized service of endpoint get /search

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,23 +1,67 @@
 import githubService from '../clients/github/github.service';
 import { ISearchBodyParams } from '../controllers/interfaces/ISearchBodyParams';
-import { ISearchResponseShape } from '../controllers/interfaces/ISearchResponseShape';
+import { ISearchResponseShape, Results } from '../controllers/interfaces/ISearchResponseShape';
+import redisClient from '../redis/redis.client';
+
+const cache = async (bodyParams: ISearchBodyParams, value: Results) => {
+  const {
+    type,
+    text,
+    page,
+    limit,
+  } = bodyParams;
+  const threeHours = 3 * 60 * 60;
+
+  return redisClient.setEx(`${type}:${text}:${page}:${limit}`, threeHours, JSON.stringify(value));
+};
+
+const getCachedData = async (
+  bodyParams: ISearchBodyParams,
+): Promise<Results | null> => {
+  const {
+    type,
+    text,
+    page,
+    limit,
+  } = bodyParams;
+
+  const cachedData = await redisClient.get(`${type}:${text}:${page}:${limit}`);
+
+  return JSON.parse((cachedData || null) as string);
+};
+
+const setSearchResultResponse = (
+  results: Results,
+  limit: number,
+  remainingRequests: number,
+): ISearchResponseShape => ({
+  hasNextPage: results.length === limit,
+  resultsCount: results.length,
+  rateLimit: {
+    remaining: remainingRequests,
+  },
+  results,
+});
 
 export default {
-  async search(bodyParams: ISearchBodyParams) {
+  async search(bodyParams: ISearchBodyParams): Promise<ISearchResponseShape> {
     const { limit } = bodyParams;
 
-    const {
-      searchResults: results, remainingRequests,
-    } = await githubService.searchGithub(bodyParams);
+    const remainingRequests = await githubService.getRemainingRequests();
 
-    const finalResponseShape: ISearchResponseShape = {
-      hasNextPage: results.length === limit,
-      resultsCount: results.length,
-      rateLimit: {
-        remaining: remainingRequests,
-      },
-      results,
-    };
-    return finalResponseShape;
+    const cachedResults = await getCachedData(bodyParams);
+    if (cachedResults) {
+      return setSearchResultResponse(cachedResults, limit, remainingRequests);
+    }
+
+    if (remainingRequests === 0) {
+      return setSearchResultResponse([], limit, remainingRequests);
+    }
+
+    const results = await githubService.searchGithub(bodyParams);
+
+    cache(bodyParams, results);
+
+    return setSearchResultResponse(results, limit, remainingRequests);
   },
 };
